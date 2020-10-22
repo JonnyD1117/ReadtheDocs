7e857f87dee33f199d4ce17516ac3020ad3cfe4a
jtdorsey22 <jonathandorsey1117@gmail.com>
2020-10-22 01:55:46 -0700

    ioasdfaskjdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./source/'))
+sys.path.insert(0, os.path.abspath('.'))
 
-auto_mock_imports = ['gym', 'numpy', 'torch',]
+auto_mock_imports = ['gym', 'numpy', 'torch']
 
 
 # -- Project information -----------------------------------------------------
